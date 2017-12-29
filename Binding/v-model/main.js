de31bdc8fd6d42c47bb4b65dfbded7beb40049d3
rizkimufti <you@example.com>
2017-12-30 03:52:19 +0700

    latihan v-model

---
+++ Binding/v-model/main.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+
+  new Vue({
+    el: '#app2',
+    data: {
+      message: 'Hello Rizki!'
+    }
+  })
+  new Vue({
+    el: '#app3',
+    data: {
+      message: 'Hello World!'
+    }
+  })
