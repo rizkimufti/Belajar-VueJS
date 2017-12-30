5ab8be16e3d9fdada91ac74d38297b159b284b8d
rizkimufti <you@example.com>
2017-12-31 00:00:56 +0700

    sinau v-text ,v-model

---
+++ Binding/v-html/main.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+      messageHTML: '<p>Hello <strong>Vue!</strong></p>'
+    }
+  })
+
+  new Vue({
+    el: '#app2',
+    data: {
+      message: 'Hello Rizki!'
+    }
+  })
